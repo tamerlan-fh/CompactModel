1ef3754be4f78ece8d84ad817430fd2af1c33ce5
tamerlan-fh <tamerlan-fh@ya.ru>
2019-06-12 15:34:43 +0500

    Добавьте файлы проекта.

---
+++ CompactModel/Views/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace CompactModel.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
