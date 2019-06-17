602770fee2dcd3d17ed2d5f5b61d19eaa8f28cc9
tamerlan-fh <tamerlan-fh@ya.ru>
2019-06-17 08:09:19 +0500

    *

--- CompactModel/ViewModels/GraphViewModel.cs
+++ CompactModel/ViewModels/GraphViewModel.cs
@@ -116,7 +116,7 @@ namespace CompactModel.ViewModels
                         dc.DrawEllipse(null, pen, point + _vector, _radius, _radius);
                         DrawArrow(dc, point + _v, new Vector(0, -1));
 
-                        var textNoose = new FormattedText($"[{string.Join(";", nose.Select(x => x.ActivePredicateSign))}]\r\n[{string.Join(";", nose.Select(x => x.CSign))}]",
+                        var textNoose = new FormattedText($"{nose.Max(x => x.Priority)}\r\n[{string.Join(";", nose.Select(x => x.ActivePredicateSign))}]\r\n[{string.Join(";", nose.Select(x => x.CSign))}]",
                             CultureInfo.CurrentCulture, FlowDirection.LeftToRight, new Typeface("Verdana"), mFont, pen.Brush, 60);
                         dc.DrawText(textNoose, point + new Vector(-1.5 * radius, 2 * radius));
                     }
@@ -136,7 +136,7 @@ namespace CompactModel.ViewModels
                             var end = point + vNextNode - vRadius;
                             dc.DrawLine(pen, start, end);
                             DrawArrow(dc, end, new Vector(vNextNode.X, vNextNode.Y));
-                            var textArc = new FormattedText($"[{string.Join(";", arc.Select(x => x.ActivePredicateSign))}]\r\n[{string.Join(";", arc.Select(x => x.CSign))}]",
+                            var textArc = new FormattedText($"{arc.Max(x => x.Priority)}\r\n[{string.Join(";", arc.Select(x => x.ActivePredicateSign))}]\r\n[{string.Join(";", arc.Select(x => x.CSign))}]",
                             CultureInfo.CurrentCulture, FlowDirection.LeftToRight, new Typeface("Verdana"), mFont, pen.Brush, 60);
                             dc.DrawText(textArc, point + new Vector(1.3 * radius, 0.3 * radius));
                         }
